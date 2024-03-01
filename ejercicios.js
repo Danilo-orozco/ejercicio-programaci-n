81a82906aefcf38771ff195703893a8bf3aba66b
Danilo-orozco <161758490+Danilo-orozco@users.noreply.github.com>
2024-03-01 09:43:15 -0500

    mi primer commit

---
+++ ejercicios.js
@@ -0,0 +1,13 @@
+var nombre="danilo";
+var apellido="orozco";
+var edad=17;
+var numero1=5;
+var numero2=3;
+var suma= numero1+numero2;
+console.log(suma);
+var tengonovia=true;
+console.log(tengonovia);
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tengonovia));
+let nombre="danilo";
