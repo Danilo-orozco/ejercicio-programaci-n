4a79ef702f35cc10c8d79f36828d5328c77b5d85
Danilo-orozco <161758490+Danilo-orozco@users.noreply.github.com>
2024-03-08 09:58:44 -0500

    tablas de multiplicar usando for

--- ciclos.js
+++ ciclos.js
@@ -47,11 +47,23 @@ for(let i = 0 ; i <= 9 ; i++){
 for(let i=1; i<=50; i++) {
     console.log(i);
 }
-*/
+
+
+
 for(let i = 0;i<=10; i++){
     console.log(i*2);
 }
 
+*/
+// tabla multiplicar del 1 al 10
+for( let i = 1;  i<= 10; i++){
+    console.log("tabla de multiplicar del", i);
+    for(let j= 1 ;j <= 10 ; j++){
+        console.log(i + "x" + j + "="+(i*j));
+  
+    }
+    console.log("\n");
 
+}
 
 
