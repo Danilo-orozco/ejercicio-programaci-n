b2e4ac1bb36a30a308a070249fdf652ce24779f0
Danilo-orozco <161758490+Danilo-orozco@users.noreply.github.com>
2024-03-08 09:32:11 -0500

    trabajo con el ciclo for

---
+++ ciclos.js
@@ -0,0 +1,57 @@
+// repetir algun codigo secuencialmente
+//iterar n cantidad de veces
+
+// mostrar los numeros del 1 al 20
+// ciclo for
+
+// donde empezamos y hasta donde queremos 
+
+//variable de control
+
+/*
+for(let i=1; i<=20; i++ ) {
+    console.log(i)
+}
+
+//do while
+
+let numero ;
+do{
+    numero = prompt ("ingresar un numero que 10");
+}
+
+while ( numero<10)
+
+// como mostar un arreglo usando un ciclo for
+
+let hombres = ["felipe","danilo","stywart","brayan"];
+console.log =[]
+
+for(let i = 0; i < hombres.length; i++){
+    console.log(hombres[i]);
+
+}
+
+//tabla de multiplicar
+*/
+
+/*
+let numerotabla = parseInt(prompt ("ingrese el numero a tabla"))
+for(let i = 0 ; i <= 9 ; i++){
+    console.log(" 2 x",i, " =", 2 * i) 
+
+}
+*/
+
+/*
+for(let i=1; i<=50; i++) {
+    console.log(i);
+}
+*/
+for(let i = 0;i<=10; i++){
+    console.log(i*2);
+}
+
+
+
+
